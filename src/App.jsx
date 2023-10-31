fbc83bd240f108812564a9b8242dcf53cd37a621
Edwin Balbuena <edwinbalbuena189@gmail.com>
2023-10-31 01:40:43 +0000

    Cambios a redes sociales

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Edwin Balbuena",
+  title: "Programador y Embajador en Microsoft Learn Student Ambassadors",
+  email: "edwinbalbuena189@gmail.com",
+  gitHub: "xPshycho",
+  instagram: "balbuena.edwinn",
+  linkedIn: "edwinbalbuena",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
