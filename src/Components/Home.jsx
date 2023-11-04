8d06973016ef3e9adba5bc3ba78b9c76c2b312ff
Edwin Balbuena <edwinbalbuena189@gmail.com>
2023-11-04 19:12:42 +0000

    Update images and alt text

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/texture.jpg";
+import image from "../images/bp.jpg";
 
-const imageAltText = "Map of the world";
+const imageAltText = "Dominican Republic beach";
 
 const Home = ({ name, title }) => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/crab.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "A crab on the beach";
 
 /**
  * Project list
